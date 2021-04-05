f21f8cc71663c049aa9d28dce3584cf3455641b0
Jonathon Béasse <crypticspaghetti@protonmail.com>
2021-04-05 10:16:20 -0600

    Extract Preview component

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,13 @@ import * as esbuild from "esbuild-wasm";
 import React, { useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom";
 import CodeEditor from "./components/code-editor";
+import Preview from "./components/preview";
 import { fetchPlugin } from "./plugins/fetch-plugin";
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 const App = () => {
   const ref = useRef<any>();
-  const iframe = useRef<any>();
+  const [code, setCode] = useState("");
   const [input, setInput] = useState("");
 
   const startService = async () => {
@@ -25,8 +26,6 @@ const App = () => {
   const onClick = async () => {
     if (!ref.current) return;
 
-    iframe.current.srcdoc = html;
-
     const result = await ref.current.build({
       entryPoints: ["index.js"],
       bundle: true,
@@ -38,29 +37,8 @@ const App = () => {
       },
     });
 
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
+    setCode(result.outputFiles[0].text);
   };
-
-  const html = `
-    <html>
-      <head></head>
-      <body>
-        <div id="root"></div>
-        <script>
-          window.addEventListener("message", (event) => {
-            try {
-              eval(event.data);
-            } catch (error) {
-              const root = document.querySelector("#root");
-              root.innerHTML = "<div style='color: red'><h4>Runtime Error</h4>" + error + "</div>";
-
-              console.error(error);
-            }
-          }, false);
-        </script>
-      </body>
-    </html>
-  `;
 
   return (
     <>
@@ -68,15 +46,10 @@ const App = () => {
         initialValue="const a = 1;"
         onChange={(value) => setInput(value)}
       />
+      <Preview code={code} />
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <iframe
-        ref={iframe}
-        srcDoc={html}
-        sandbox="allow-scripts"
-        title="preview"
-      />
     </>
   );
 };
