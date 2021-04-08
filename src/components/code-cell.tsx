bc471ff72055cb79fe293de2eea7583a684b58fa
Jonathon Béasse <crypticspaghetti@protonmail.com>
2021-04-07 21:43:12 -0600

    Add better error handling

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -7,11 +7,13 @@ import Resizable from "./resizable";
 const CodeCell = () => {
   const [code, setCode] = useState("");
   const [input, setInput] = useState("");
+  const [err, setErr] = useState("");
 
   useEffect(() => {
     const timer = setTimeout(async () => {
       const output = await bundle(input);
-      setCode(output);
+      setCode(output.code);
+      setErr(output.error);
     }, 1000);
 
     return () => {
@@ -28,7 +30,7 @@ const CodeCell = () => {
             onChange={(value) => setInput(value)}
           />
         </Resizable>
-        <Preview code={code} />
+        <Preview code={code} bundlingStatus={err} />
       </div>
     </Resizable>
   );
--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -3,6 +3,7 @@ import "./preview.css";
 
 interface PreviewProps {
   code: string;
+  bundlingStatus: string;
 }
 
 const html = `
@@ -15,14 +16,20 @@ const html = `
     <body>
       <div id="root"></div>
       <script>
+        const handleError = (error) => {
+          const root = document.querySelector("#root");
+          root.innerHTML = "<div style='color: red'><h4>Runtime Error</h4>" + error + "</div>";
+          console.error(error);
+        };
+        window.addEventListener("error", (event) => {
+          event.preventDefault();
+          handleError(event.error);
+        })
         window.addEventListener("message", (event) => {
           try {
             eval(event.data);
           } catch (error) {
-            const root = document.querySelector("#root");
-            root.innerHTML = "<div style='color: red'><h4>Runtime Error</h4>" + error + "</div>";
-
-            console.error(error);
+            handleError(error);
           }
         }, false);
       </script>
@@ -30,7 +37,7 @@ const html = `
   </html>
 `;
 
-const Preview: React.FC<PreviewProps> = ({ code }) => {
+const Preview: React.FC<PreviewProps> = ({ code, bundlingStatus }) => {
   const iframe = useRef<any>();
 
   useEffect(() => {
@@ -48,6 +55,7 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
         sandbox="allow-scripts"
         title="preview"
       />
+      {bundlingStatus && <div className="preview-error">{bundlingStatus}</div>}
     </div>
   );
 };
